7a63c14db4aad476fab588d5d1fe9c5ccacbf1fb
sorspetar111 <156424190+sorspetar111@users.noreply.github.com>
2024-01-12 12:43:19 +0200

    Add files via upload

---
+++ SirmaApp/Employers.Net/EmployeeProjectRecordMap.cs
@@ -0,0 +1,17 @@
+﻿using CsvHelper.Configuration;
+
+namespace Employers.Net
+{
+    class EmployeeProjectRecordMap : ClassMap<EmployeeProjectRecord>
+    {
+        public EmployeeProjectRecordMap()
+        {
+            Map(m => m.EmpID).Name("EmpID");
+            Map(m => m.ProjectID).Name("ProjectID");
+            Map(m => m.DateFrom).Name("DateFrom").TypeConverterOption.Format("yyyy-MM-dd");
+            Map(m => m.DateTo).Name("DateTo").TypeConverterOption.Format("yyyy-MM-dd");
+        }
+    }
+
+
+}
