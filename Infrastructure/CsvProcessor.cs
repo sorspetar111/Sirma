bb799ec0b9c63a09c9b4e67765a2d7b0c66c8f94
Petar J. <156424190+sorspetar111@users.noreply.github.com>
2025-11-03 23:21:10 +0200

    Add files via upload

--- Infrastructure/CsvProcessor.cs
+++ Infrastructure/CsvProcessor.cs
@@ -1,8 +1,9 @@
 ﻿namespace Infrastructure
 {
     using CsvHelper;
+    using CsvHelper.Configuration;
     using Domains.Models;
-    using System.Collections.Generic; 
+    using System.Collections.Generic;
     using System.Globalization;
     using System.IO;
 
@@ -13,6 +14,31 @@
             using var reader = new StreamReader(filePath);
             using var csv = new CsvReader(reader, CultureInfo.InvariantCulture);
             return csv.GetRecords<EmployeeProject>().ToList();
+        }
+    }
+
+
+    public class CsvSafeProcessor
+    {
+        public static List<EmployeeProject> DeserializeFromCsv(string filePath)
+        {
+            using var reader = new StreamReader(filePath);
+            using var csv = new CsvReader(reader, CultureInfo.InvariantCulture);
+
+            csv.Context.RegisterClassMap<EmployeeProjectMap>();
+            return csv.GetRecords<EmployeeProject>().ToList();
+        }
+    }
+
+    public sealed class EmployeeProjectMap : ClassMap<EmployeeProject>
+    {
+        public EmployeeProjectMap()
+        {
+            Map(m => m.EmpID).Index(0);
+            Map(m => m.ProjectID).Index(1);
+            Map(m => m.DateFrom).Index(2).Convert(args => DateTimeExtensions.ToSafeDateTime(args.Row.GetField(2)));
+            Map(m => m.DateTo).Index(3).Convert(args => DateTimeExtensions.ToSafeDateTime(args.Row.GetField(3)));
+
         }
     }
 
---
+++ Infrastructure/DateTimeExtensions.cs
@@ -0,0 +1,40 @@
+﻿using CsvHelper;
+using CsvHelper.Configuration;
+using System.Globalization;
+
+namespace Infrastructure
+{
+    public static class DateTimeExtensions
+    {
+        private static readonly string[] SupportedFormats =
+        {
+            "yyyy-MM-dd",
+            "dd.MM.yyyy",
+            "dd/M/yy",
+            "MM/d/yy"
+        };
+
+        public static DateTime ToSafeDateTime(string? date)
+        {
+            if (string.IsNullOrWhiteSpace(date) || date.Trim().Equals("NULL", StringComparison.OrdinalIgnoreCase))
+                return DateTime.Now;
+
+            if (DateTime.TryParseExact(date.Trim(), SupportedFormats, CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime result))
+                return result;
+
+            return DateTime.Now;
+        }
+
+    }
+
+
+    public class SafeDateTimeConverter : CsvHelper.TypeConversion.DateTimeConverter
+    {
+        public override object ConvertFromString(string text, IReaderRow row, MemberMapData memberMapData)
+        {
+            return DateTimeExtensions.ToSafeDateTime(text);
+        }
+    }
+
+
+}
